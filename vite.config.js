4ee28460e2a919dbf803d8b42ee93c0a5d27196b
tehstormcrow <ibrahimayhankus@gmail.com>
2025-10-28 16:27:48 +0300

    fix(deploy): add public path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/https://tehstormcrow.github.io/merit-shipping/"
+      : "/",
+  plugins: [tailwindcss()],
 });
