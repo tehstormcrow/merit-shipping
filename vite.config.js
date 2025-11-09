31a72e717b31ce73c8623a5de22ef8ab531df2ba
tehstormcrow <ibrahimayhankus@gmail.com>
2025-11-09 22:25:53 +0300

    feature(merit-shipping): update the path for css

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base:
     process.env.NODE_ENV === "production"
-      ? "https://tehstormcrow.github.io/merit-shipping/"
+      ? "https://www.meritshipping.net/"
       : "/",
   plugins: [tailwindcss()],
 });
